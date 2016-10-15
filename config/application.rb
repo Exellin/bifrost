a2728420b879ead24649ab7677b5e1f4beb3a185
Exellin <carnegieshawn@yahoo.ca>
2016-10-15 10:03:46 +0000

    set asset initialization on precompile to false

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module Bifrost
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += %W(#{config.root}/lib)
     config.serve_static_assets = true
+    config.assets.initialize_on_precompile = false
   end
 end
